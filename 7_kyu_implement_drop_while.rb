a054c58a7cb00367c9bf194f10803981995d6da2
Tien Do <domanhtien2011@gmail.com>
2020-08-27 03:20:11 +0700

    Implement drop_while method

---
+++ 7_kyu_implement_drop_while.rb
@@ -0,0 +1,19 @@
+# Create a method drop_while that accepts a list and a block, and returns a copy of the list that skips over elements from the left, for as long as the given block returns true.
+
+# Time complexity: O(n)
+# Space complexity: O(n)
+# I know there is a built-in drop_while method in the Enumerable Module. Just wanna implement it for fun
+
+def drop_while(list, &block)
+  return list if list.empty?
+
+  starting_index = 0
+  list.each_with_index do |ele, index|
+    if block.call(ele) == false
+      starting_index = index
+      break
+    end
+  end
+
+  list[starting_index, list.count - 1]
+end
