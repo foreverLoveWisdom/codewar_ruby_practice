8186f36ce1675ac22ece5ea88db777d7ab2a5d36
Tien Do <domanhtien2011@gmail.com>
2020-08-26 23:40:09 +0700

    Are they the same?

---
+++ 6_kyu_are_they_the_same.rb
@@ -0,0 +1,9 @@
+# Given two arrays a and b write a function comp(a, b) (compSame(a, b) in Clojure) that checks whether the two arrays have the "same" elements, with the same multiplicities. "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+def comp(array1, array2)
+  return false if array1.nil? || array2.nil?
+
+  array1.map { |num| num**2 }.sort == array2.sort
+end
